a86b12d9d5d296330490b4973dfb521cead16d57
Fomagran <fomagran6@gmail.com>
2023-02-20 20:03:55 -0600

    Glyphs

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -1,7 +1,8 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random')
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080]
 };
 
 let manager;
@@ -14,19 +15,19 @@ const typeContext = typeCanvas.getContext('2d')
 
 const sketch = ({ context, width, height }) => {
   const cell = 20
-  const cols = Math.floor(width/cell)
-  const rows = Math.floor(height/cell)
+  const cols = Math.floor(width / cell)
+  const rows = Math.floor(height / cell)
   const numCells = cols * rows
 
   typeCanvas.width = cols
   typeCanvas.height = rows
 
   return ({ context, width, height }) => {
- 
+
     typeContext.fillStyle = 'black';
     typeContext.fillRect(0, 0, cols, rows);
 
-    fontSize = cols
+    fontSize = cols * 1.2
 
     typeContext.fillStyle = 'white'
     typeContext.font = fontSize + 'px ' + fontFamily
@@ -41,45 +42,65 @@ const sketch = ({ context, width, height }) => {
     const tx = (cols - mw) * 0.5 - mx
     const ty = (rows - mh) * 0.5 - my
     typeContext.save()
-    typeContext.translate(tx,ty)
+    typeContext.translate(tx, ty)
 
     typeContext.beginPath()
-    typeContext.rect(mx,my,mw,mh)
+    typeContext.rect(mx, my, mw, mh)
     typeContext.stroke()
 
-    typeContext.fillText(text,0,0)
+    typeContext.fillText(text, 0, 0)
     typeContext.restore()
 
-    const typeData = typeContext.getImageData(0,0,cols,rows).data
-    context.drawImage(typeCanvas,0,0)
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data
+    context.fillStyle = 'black'
+    context.fillRect(0, 0, width, height)
+    context.textBaseline = 'middle'
+    context.textAlign = 'center'
+    // context.drawImage(typeCanvas, 0, 0)
 
     for (let i = 0; i < numCells; i++) {
-       const col = i%cols
-       const row = Math.floor(i/cols)
+      const col = i % cols
+      const row = Math.floor(i / cols)
 
-       const x = col * cell
-       const y = row * cell
+      const x = col * cell
+      const y = row * cell
 
-       const r = typeData[i*4+0]
-       const g = typeData[i*4+1]
-       const b = typeData[i*4+2]
-       const a = typeData[i*4+3]
+      const r = typeData[i * 4 + 0]
+      const g = typeData[i * 4 + 1]
+      const b = typeData[i * 4 + 2]
+      const a = typeData[i * 4 + 3]
 
-       context.fillStyle = `rgb(${r}, ${g}, ${b})`
-       context.save()
-       context.translate(x,y)
-       context.fillRect(0,0,cell,cell)
-       context.restore()
+      const glyph = getGlyph(r)
+
+      context.font = `${cell * 2}px ${fontFamily}`
+      if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`
+      context.fillStyle = `rgb(${r}, ${g}, ${b})`
+      context.save()
+      context.translate(x, y)
+      //  context.fillRect(0,0,cell,cell)
+      context.fillText(glyph, 0, 0)
+
+      context.restore()
     }
   };
 };
+
+const getGlyph = (v) => {
+    if (v < 50) return ''
+    if (v < 100) return '.'
+    if (v < 150) return '-'
+    if (v < 200) return '+'
+
+    const glyphs = '_= /'.split('')
+    return random.pick(glyphs)
+}
 
 const onKeyUp = (e) => {
   text = e.key.toUpperCase()
   manager.render()
 }
 
-document.addEventListener('keyup',onKeyUp)
+document.addEventListener('keyup', onKeyUp)
 
 
 const start = async () => {
